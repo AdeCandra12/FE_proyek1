dde6d969bb963711021da54448bf631e27a2e9a8
AdeCandra12 <ade.can321@gmail.com>
2023-08-20 20:04:40 +0700

    add

--- js/temp/table.js
+++ js/temp/table.js
@@ -1,6 +1,6 @@
 export let isiTabel =
 `
-<tr class="h-18 border-b border-coolGray-100">
+<tr class="h-18 border-b border-darkcoolGray-100">
   <th class="whitespace-nowrap px-4 bg-white text-left">
     <div class="flex items-center -m-2">
       <div class="w-auto p-2">
@@ -20,11 +20,9 @@ export let isiTabel =
       </div>
       <div class="w-auto p-2">
         <p class="text-xs font-semibold text-coolGray-800">#NAMAMK#</p>
-        <p class="text-xs font-medium text-coolGray-600">#SKS#</p>
-        <p class="text-xs font-medium text-coolGray-600">#DOSEN#</p>
-        <p class="text-xs font-medium text-coolGray-600">#EMAILDOSEN#</p>
-        <p class="text-xs font-medium text-coolGray-600">#TANGGAL#</p>
-        <p class="text-xs font-medium text-coolGray-600">#CHECKIN#</p>
+        <p class="text-xs font-medium text-coolGray-500">#SKS#</p>
+        <p class="text-xs font-medium text-coolGray-500">#DOSEN#</p>
+        <p class="text-xs font-medium text-coolGray-500">#EMAILDOSEN#</p>
       </div>
     </div>
     <th class="whitespace-nowrap px-4 bg-white text-left">
