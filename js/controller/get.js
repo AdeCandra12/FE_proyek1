bd54eb55af34cb9d4410e4706d1ad188eddfbbab
AdeCandra12 <ade.can321@gmail.com>
2023-08-20 19:03:55 +0700

    add

--- js/controller/get.js
+++ js/controller/get.js
@@ -1,26 +1,3 @@
-import { addInner } from "https://bukulapak.github.io/element/process.js";
-import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
-import { isiTabel } from "../temp/table.js";
-export function isiTabelNilai(results) {
-    results.forEach(isiRow);
-}
-function isiRow(value) {
-    let content = isiTabel
-      .replace('#NAMAMHS#', value.npm_ms.nama_mhs ? value.npm_ms.nama_mhs : '#NAMAMHS#')
-      .replace('#NPM#', value.npm_ms ? value.npm_ms.npm : '#NPM#')
-      .replace('#JURUSAN#', value.npm_ms.jurusan ? value.npm_ms.jurusan : '#JURUSAN#')
-      .replace('#EMAIL#', value.npm_ms.email ? value.npm_ms.email : '#EMAIL#')
-      .replace('#NAMAMK#', value.presensi.nama_mk ? value.presensi.nama_mk.nama_matkul : '#NAMAMK#')
-      .replace('#SKS#', value.presensi.nama_mk ? value.presensi.nama_mk.sks : '#SKS#')
-      .replace('#DOSEN#', value.presensi.nama_mk ? value.presensi.nama_mk.dosen_pengampu : '#DOSEN#')
-      .replace('#EMAILDOSEN#', value.presensi.nama_mk ? value.presensi.nama_mk.email : '#EMAILDOSEN#')
-      .replace('#NAKHIR#', value.nilai_akhir)
-      .replace('#GRADE#', value.grade)
-      .replace('#THAJARAN#', value.tahun_ajaran)
-      .replace('#WARNA#', getRandomColor())
-      .replace(/#WARNALOGO#/g, getRandomColorName());
-    addInner('iniTabel', content);
-}
 // import { addInner } from "https://bukulapak.github.io/element/process.js";
 // import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
 // import { isiTabel } from "../temp/table.js";
@@ -28,14 +5,44 @@ function isiRow(value) {
 //     results.forEach(isiRow);
 // }
 // function isiRow(value) {
-//     let content =
-//         isiTabel.replace("#NAMAMHS#", value.npm_ms.nama_mhs)
-//             .replace("#NPM#", value.npm_ms.npm)
-//             .replace("#JURUSAN#", value.npm_ms.jurusan)
-//             .replace("#EMAIL#", value.npm_ms.email)
-//             .replace("#STATUS#", value.checkin)
-//             .replace("#HARIKERJA#", value.biodata.hari_kerja)
-//             .replace("#WARNA#", getRandomColor())
-//             .replace(/#WARNALOGO#/g, getRandomColorName());
-//     addInner("iniTabel", content);
+//     let content = isiTabel
+//       .replace('#NAMAMHS#', value.npm_ms.nama_mhs ? value.npm_ms.nama_mhs : '#NAMAMHS#')
+//       .replace('#NPM#', value.npm_ms ? value.npm_ms.npm : '#NPM#')
+//       .replace('#JURUSAN#', value.npm_ms.jurusan ? value.npm_ms.jurusan : '#JURUSAN#')
+//       .replace('#EMAIL#', value.npm_ms.email ? value.npm_ms.email : '#EMAIL#')
+//       .replace('#NAMAMK#', value.presensi.nama_mk ? value.presensi.nama_mk.nama_matkul : '#NAMAMK#')
+//       .replace('#SKS#', value.presensi.nama_mk ? value.presensi.nama_mk.sks : '#SKS#')
+//       .replace('#DOSEN#', value.presensi.nama_mk ? value.presensi.nama_mk.dosen_pengampu : '#DOSEN#')
+//       .replace('#EMAILDOSEN#', value.presensi.nama_mk ? value.presensi.nama_mk.email : '#EMAILDOSEN#')
+//       .replace('#NAKHIR#', value.nilai_akhir)
+//       .replace('#GRADE#', value.grade)
+//       .replace('#THAJARAN#', value.tahun_ajaran)
+//       .replace('#WARNA#', getRandomColor())
+//       .replace(/#WARNALOGO#/g, getRandomColorName());
+//     addInner('iniTabel', content);
 // }
+import { addInner } from "https://bukulapak.github.io/element/process.js";
+import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+import { isiTabel } from "../temp/table.js";
+export function isiTabelNilai(results) {
+    results.forEach(isiRow);
+}
+function isiRow(value) {
+    let content =
+        isiTabel.replace("#NAMAMHS#", value.npm_ms.nama_mhs)
+            .replace("#NPM#", value.npm_ms.npm)
+            .replace("#JURUSAN#", value.npm_ms.jurusan)
+            .replace("#EMAIL#", value.npm_ms.email)
+            .replace("#NAMAMK#", value.presensi.nama_mk.nama_matkul)
+            .replace("#SKS#", value.presensi.nama_mk.sks)
+            .replace("#DOSEN#", value.presensi.nama_mk.dosen_pengampu)
+            .replace("#EMAILDOSEN#", value.presensi.nama_mk.email)
+            .replace("#TANGGAL#", value.presensi.nama_mk.tanggal)
+            .replace("#CHECKIN#", value.presensi.nama_mk.checkin)
+            .replace("#NAKHIR#", value.presensi.nama_mk.sks)
+            .replace("#GRADE#", value.presensi.grade)
+            .replace("#THAJARAN#", value.presensi.tahun_ajaran)
+            .replace("#WARNA#", getRandomColor())
+            .replace(/#WARNALOGO#/g, getRandomColorName());
+    addInner("iniTabel", content);
+}
--- js/temp/table.js
+++ js/temp/table.js
@@ -27,8 +27,21 @@ export let isiTabel =
         <p class="text-xs font-medium text-coolGray-200">#SKS#</p>
         <p class="text-xs font-medium text-coolGray-200">#DOSEN#</p>
         <p class="text-xs font-medium text-coolGray-200">#EMAILDOSEN#</p>
+        <p class="text-xs font-medium text-coolGray-200">#TANGGAL#</p>
+        <p class="text-xs font-medium text-coolGray-200">#CHECKIN#</p>
       </div>
     </div>
+    <th class="whitespace-nowrap px-4 bg-white text-left">
+    <div class="flex items-center -m-2">
+      <div class="w-auto p-2">
+        <input class="w-4 h-4 bg-white rounded" type="checkbox">
+      </div>
+      <div class="w-auto p-2">
+        <p class="text-xs font-semibold text-coolGray-800">#TANGGAL#</p>
+        <p class="text-xs font-medium text-coolGray-500">#CHECKIN#</p>
+      </div>
+    </div>
+  </th>
   </th>
   <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-left">
     #NAKHIR#</th>
