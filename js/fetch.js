bd4f72900281b523ae53d6807a54aa8b8e15c84f
AdeCandra12 <ade.can321@gmail.com>
2023-08-20 12:49:00 +0700

    add

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,11 @@
-import { get } from "https://bukulapak.github.io/api/process.js"; 
+import { get } from "https://bukulapak.github.io/api/process.js";
+import { setInner } from "https://bukulapak.github.io/element/process.js";
 let urlAPI = "https://pemrog3-ulbi.herokuapp.com/mahasiswa";
 get(urlAPI,isiTableMahasiswa);
 function isiTableMahasiswa(results){
     console.log(results);
+    results.forEach(isiRow);
+}
+function isiRow(value){
+    console.log(value)
 }
