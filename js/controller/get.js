3e3f5fcd596c7ab37894dcac24948aa39a9329b8
AdeCandra12 <ade.can321@gmail.com>
2023-08-20 19:14:00 +0700

    add

--- js/controller/get.js
+++ js/controller/get.js
@@ -23,28 +23,3 @@ function isiRow(value) {
       .replace(/#WARNALOGO#/g, getRandomColorName());
     addInner('iniTabel', content);
 }
-// import { addInner } from "https://bukulapak.github.io/element/process.js";
-// import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
-// import { isiTabel } from "../temp/table.js";
-// export function isiTabelNilai(results) {
-//     results.forEach(isiRow);
-// }
-// function isiRow(value) {
-//     let content =
-//         isiTabel.replace("#NAMAMHS#", value.npm_ms.nama_mhs)
-//             .replace("#NPM#", value.npm_ms.npm)
-//             .replace("#JURUSAN#", value.npm_ms.jurusan)
-//             .replace("#EMAIL#", value.npm_ms.email)
-//             .replace("#NAMAMK#", value.presensi.nama_mk.nama_matkul)
-//             .replace("#SKS#", value.presensi.nama_mk.sks)
-//             .replace("#DOSEN#", value.presensi.nama_mk.dosen_pengampu)
-//             .replace("#EMAILDOSEN#", value.presensi.nama_mk.email)
-//             .replace("#TANGGAL#", value.presensi.nama_mk.tanggal)
-//             .replace("#CHECKIN#", value.presensi.nama_mk.checkin)
-//             .replace("#NAKHIR#", value.presensi.nama_mk.sks)
-//             .replace("#GRADE#", value.presensi.grade)
-//             .replace("#THAJARAN#", value.presensi.tahun_ajaran)
-//             .replace("#WARNA#", getRandomColor())
-//             .replace(/#WARNALOGO#/g, getRandomColorName());
-//     addInner("iniTabel", content);
-// }
