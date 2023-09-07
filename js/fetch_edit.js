38163b863657441a2f16ce6db2cd28806d4a4ffe
AdeCandra12 <ade.can321@gmail.com>
2023-09-07 14:37:47 +0700

    add

--- js/fetch_edit.js
+++ js/fetch_edit.js
@@ -1,5 +1,5 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
-import { isiDataNilai } from "./controller/edit_nilai";
-import { urlFetch } from "./config/url_get_nilai";
+import { isiDataNilai } from "./controller/edit_nilai.js";
+import { urlFetch } from "./config/url_get_nilai.js";
 
 get(urlFetch, isiDataNilai)
